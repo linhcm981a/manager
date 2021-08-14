bab702fe34b8eb0953b396f7aaa6351cb9fa6ded
name <linhcm99999@gmail.com>
2021-08-14 17:52:05 +0700

    first commit

---
+++ client/src/components/body/card/Detail.js
@@ -0,0 +1,161 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import {useSelector, useDispatch} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {isLength, isMatch} from '../../utils/validation/Validation'
+import {showSuccessMsg, showErrMsg} from '../../utils/notification/Notification'
+import {fetchAllUsers, dispatchGetAllUsers} from '../../../redux/actions/usersAction'
+
+const initialState = {
+    name: '',
+    password: '',
+    cf_password: '',
+    err: '',
+    success: ''
+}
+
+function Detail() {
+    const auth = useSelector(state => state.auth)
+    const token = useSelector(state => state.token)
+
+    const users = useSelector(state => state.users)
+
+    const {user, isAdmin} = auth
+    const [data, setData] = useState(initialState)
+    const {name, password, cf_password, err, success} = data
+
+  
+    const [loading, setLoading] = useState(false)
+    const [callback, setCallback] = useState(false)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(isAdmin){
+            fetchAllUsers(token).then(res =>{
+                dispatch(dispatchGetAllUsers(res))
+            })
+        }
+    },[token, isAdmin, dispatch, callback])
+
+    const handleChange = e => {
+        const {name, value} = e.target
+        setData({...data, [name]:value, err:'', success: ''})
+    }
+
+    const updateInfor = () => {
+        try {
+            axios.patch('/user/update', {
+                name: name ? name : user.name,
+                avatar: avatar ? avatar : user.avatar
+            },{
+                headers: {Authorization: token}
+            })
+
+            setData({...data, err: '' , success: "Updated Success!"})
+        } catch (err) {
+            setData({...data, err: err.response.data.msg , success: ''})
+        }
+    }
+
+
+    const handleUpdate = () => {
+        if(name || avatar) updateInfor()
+        if(password) updatePassword()
+    }
+
+    const handleDelete = async (id) => {
+        try {
+            if(user._id !== id){
+                if(window.confirm("Are you sure you want to delete this account?")){
+                    setLoading(true)
+                    await axios.delete(`/user/delete/${id}`, {
+                        headers: {Authorization: token}
+                    })
+                    setLoading(false)
+                    setCallback(!callback)
+                }
+            }
+            
+        } catch (err) {
+            setData({...data, err: err.response.data.msg , success: ''})
+        }
+    }
+
+    return (
+        <>
+        <div>
+            {err && showErrMsg(err)}
+            {success && showSuccessMsg(success)}
+            {loading && <h3>Loading.....</h3>}
+        </div>
+        <div className="profile_page">
+            <div className="col-left">
+                <h2>{isAdmin ? "Admin Detail": "User Detail"}</h2>
+
+                <div className="avatar">
+                    <img src={avatar ? avatar : user.avatar} alt=""/>
+                    <span>
+                        <i className="fas fa-camera"></i>
+                        <p>Change</p>
+                        <input type="file" name="file" id="file_up" disabled />
+                    </span>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" defaultValue={user.name}
+                    placeholder="Your name" disabled />
+                </div>
+
+
+                <button disabled={loading} onClick={handleUpdate}>Update</button>
+            </div>
+
+            <div className="col-right">
+                <h2>{isAdmin ? "Users" : "My Orders"}</h2>
+
+                <div style={{overflowX: "auto"}}>
+                    <table className="customers">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                               
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map(user => (
+                                    <tr key={user._id}>
+                                        <td>{user._id}</td>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>
+                                            {
+                                                user.role === 1
+                                                ? <i className="fas fa-check" title="Admin"></i>
+                                                : <i className="fas fa-times" title="User"></i>
+                                            }
+                                        </td>
+                                        <td>
+                                            <Link to={`/edit_user/${user._id}`}>
+                                                <i className="fas fa-edit" title="Edit"></i>
+                                            </Link>
+                                            <i className="fas fa-trash-alt" title="Remove"
+                                            onClick={() => handleDelete(user._id)} ></i>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Detail
